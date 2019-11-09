24899805654ec82b8021061c455971fa63e8a324
rishabhsingh971 <rishabhsingh971@gmail.com>
2019-11-10 01:33:45 +0530

    format lines

--- src/index.js
+++ src/index.js
@@ -11,13 +11,10 @@ class Sample extends Phaser.Scene {
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude',
-            'assets/dude.png',
-            {
-                frameWidth: 32,
-                frameHeight: 48
-            }
-        );
+        this.load.spritesheet('dude', 'assets/dude.png', {
+            frameWidth: 32,
+            frameHeight: 48
+        });
         this.load.spritesheet('explosion', 'assets/explosion.png', {
             frameWidth: 16,
             frameHeight: 16
